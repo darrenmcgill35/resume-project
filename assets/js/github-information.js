46c9c80c3ac068a6b604180f7a657550550b35c5
Ubuntu <ubuntu@ip-172-31-91-1.ec2.internal>
2019-06-13 17:49:13 +0000

    updated files

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -27,13 +27,16 @@ function fetchGitHubInformation(event) {
         `<div id="loader">
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
-        
-     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`)
+
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function(response) {
-            var userData = response;
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         },
         function(errorResponse) {
             if (errorResponse.status === 404) {
